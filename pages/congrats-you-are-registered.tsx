b7dc2712ebc9a078393156ab65e2cbc04dea9d98
Lilja Listus <lilja.listus@gmail..com>
2022-12-06 21:03:05 +0100

    update the congrats you're registered

--- pages/congrats-you-are-registered.tsx
+++ pages/congrats-you-are-registered.tsx
@@ -5,11 +5,10 @@ const CongratsYouAreRegistered: React.FC = (): JSX.Element => {
 
     return (<Container >
         <Box my={5} className={styles.congratsNewParticipant} >
-            <Typography variant="h5" component="h1" className={styles.congratsNewParticipant__title}>Congrats, you are registered for the conference. Check your mail</Typography>
+            <Typography variant="h5" component="h1" className={styles.congratsNewParticipant__title}>Congrats, you are registered for the conference. Check your mail!</Typography>
             <CardMedia
-                height="450"
-                width="100"
-                image='/baloons.jpg'
+                height="510"
+                image='/congrats.jpg'
                 title="Рускій ваєнний карабль іді ..."
                 component="img"
             />
